f6d061c5cbc935b2cb61c15626b0452f95e1b082
AbrMa <abrma98@gmail.com>
2021-04-16 12:01:40 -0500

    1.6 first aproach

---
+++ Arrays and Strings/1.6 String Compression.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string string_compression (string& str) {
+	string compressed_string = "";
+	for (int i = 0, j; i < str.size(); i = j) {
+		int repetitions = 0;	
+		for (j = i ; j < str.size(); j++){
+			if (str[i] == str[j])
+				repetitions++;
+			else break;
+		}
+		compressed_string.push_back(str[i]);
+		compressed_string.append(to_string(repetitions));
+	}
+	if (compressed_string.size() < str.size())
+		return compressed_string;
+	return str;
+}
+
+int main () {
+	string str;
+	cout << "Input -> ";
+	cin >> str;
+	cout << string_compression(str) << endl;
+	return 0;
+}
