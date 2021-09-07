fbf737cf8cd02a7d2cdf81dd89d1c7d94bb70fc7
Abrma <abrma98@gmail.com>
2021-09-07 16:06:53 -0500

    Fixed bugs

--- Stacks and Queues/3.4 Queue via Stacks.cpp
+++ Stacks and Queues/3.4 Queue via Stacks.cpp
@@ -2,20 +2,10 @@
 #include <stack>
 using namespace std;
 
-class Queue { // Only positive numbers
+class Queue { 
 	private:
 		stack <int> push_stack;
 		stack <int> pop_stack;
-		int size;
-		int front;
-		int back;
-
-		void prepare_elements () {
-			while (push_stack.empty() == false) {
-				push_stack.push(pop_stack.top());
-				push_stack.pop();
-			}
-		}
 	public:
 		bool isempty() {
 			if (push_stack.empty() && pop_stack.empty()) {
@@ -24,49 +14,32 @@ class Queue { // Only positive numbers
 			return false;
 		}
 
-		int get_back() {
-			return back;
-		}
-
-		int get_front () {
-			prepare_elements();
-			front = pop_stack.top();
-			return front;
-		}
-
 		void enqueue (int x) {
-			back = x;
 			push_stack.push(x);
 		}
 
 		int dequeue () {
-			if (isempty()) {
-				cout << "Error stack is empty" << endl;
-				return -1;
-			}
 			if (pop_stack.empty()) {
-				prepare_elements();
+				while (push_stack.empty() == false) {
+					pop_stack.push(push_stack.top());
+					push_stack.pop();
+				}
 			}
-			int x = pop_stack.top();
+			int top = pop_stack.top();
 			pop_stack.pop();
-			front = pop_stack.top();
-			return x;	
+			return top;	
 		}
 };
 
 int main () {
 	Queue q;
-	if (q.isempty())
-		cout << "concha de la madre\n";
-	q.enqueue(1);
-	q.enqueue(2);
-	q.enqueue(3);
-	q.enqueue(4);
-	q.enqueue(5);
 
-	if (!q.isempty())
-		cout << "hola madre\n";
+	for (int i = 1; i <= 5; i++) {
+		q.enqueue(i*10);
+	}
 
-	cout << q.get_front() << endl;
+	while (!q.isempty()) {
+		cout << q.dequeue() << endl;
+	}
 	return 0;
 }
